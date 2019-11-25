2f14af9b1759b692d80c0da437d96f1ce3d0f986
valentin.kozlov <valentin.kozlov@kit.edu>
2019-11-25 17:44:36 +0100

    inital commit with already existing script

---
+++ README.md
@@ -0,0 +1,10 @@
+Check DEEP-OC artifact
+============================================
+
+![DEEP-Hybrid-DataCloud logo](https://docs.deep-hybrid-datacloud.eu/en/latest/_static/logo.png)
+
+This repository is intended for a collection of scripts to test final artifacts of users contributing to the [DEEP Open Catalog](https://marketplace.deep-hybrid-datacloud.eu/).
+The scripts will be executed at the end of the Jenkins pipeline to check if the final Docker image behaves as expected.
+
+@2019
+
---
+++ check_artifact.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 - 2019 Karlsruhe Institute of Technology - Steinbuch Centre for Computing
+# This code is distributed under the MIT License
+# Please, see the LICENSE file
+#
+# @author: vykozlov
+
+### info
+# top level script to call various subscripts
+#
+# input: Docker image to start as DEEPaaS-based application
+###
+
+### Main configuration
+# Default Docker image, can be re-defiend
+DOCKER_IMAGE=deephdc/deep-oc-generic
+###
+
+### Usage message (params can be re-defined) ###
+USAGEMESSAGE="Usage: $0 <docker_image>"
+
+#### Parse input ###
+arr=("$@")
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    # print usagemessage
+    shopt -s xpg_echo
+    echo $USAGEMESSAGE
+    exit 1
+elif [ $# -eq 1 ]; then
+    DOCKER_IMAGE=$1
+else
+    # Wrong number of arguments is given (!=1)
+    echo "[ERROR] Wrong number of arguments provided!"
+    shopt -s xpg_echo
+    echo $USAGEMESSAGE
+    exit 2
+fi
+
+# Check metadata
+./check_metadata.sh $DOCKER_IMAGE
